baedee017aa9d4df350abe9dd81c9f02699b7fb3
emusute1212 <miyanishi1212@gmail.com>
2024-04-20 15:26:06 +0900

    fix: Update content

--- lib/model/repository/private_repository.dart
+++ lib/model/repository/private_repository.dart
@@ -11,15 +11,29 @@ class PrivateRepository {
       topImage: "images/top_image/private_image.jpeg",
       services: [
         PrivateCreatedServiceEntity(
-          title: "電卓アプリ",
-          detail: "通常の電卓アプリ。\nシンプルで使いやすい電卓アプリになっております。\n"
-              "数値と四則演算のボタンの形状を分けることでわかりやすく、押しやすくなっております。\n"
-              "一般的に使用する機能を網羅しています。\n"
-              "広告なしで使えます！！！\n"
+          title: "マイキー",
+          detail: "後で読む記事などを「共有」のメニューからキープするアプリです！\n"
               "（デザインはデザイナーさんに作っていただきました。）",
           link: LinkEntity(
-            imageUrl: "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
-            url: "https://play.google.com/store/apps/details?id=io.github.emusute1212.calculator",
+            imageUrl:
+                "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
+            url:
+                "https://play.google.com/store/apps/details?id=com.mykeep.yuka.mykeep",
+          ),
+          skills: [
+            "Flutter",
+            "Dart",
+          ],
+        ),
+        PrivateCreatedServiceEntity(
+          title: "電卓アプリ",
+          detail: "広告なしの電卓アプリです！\n"
+              "（デザインはデザイナーさんに作っていただきました。）",
+          link: LinkEntity(
+            imageUrl:
+                "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
+            url:
+                "https://play.google.com/store/apps/details?id=io.github.emusute1212.calculator",
           ),
           skills: [
             "Kotlin",
@@ -28,12 +42,13 @@ class PrivateRepository {
         ),
         PrivateCreatedServiceEntity(
           title: "memotte",
-          detail: "ワンスライドでサクッとメモを取ることができるアプリ。\n"
-    "日記やメモ帳、ノートや買い物リストなど、いろんなメモに有効活用できます！！\n"
-    "忙しい時でもサクッとメモが取れる、というところをメインコンセプトとして開発しました。",
+          detail: "ワンスライドでサクッとメモを取ることができるアプリです！\n"
+              "（デザインはデザイナーさんに作っていただきました。）",
           link: LinkEntity(
-            imageUrl: "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
-            url: "https://play.google.com/store/apps/details?id=io.github.emusute1212.memotte",
+            imageUrl:
+                "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
+            url:
+                "https://play.google.com/store/apps/details?id=io.github.emusute1212.memotte",
           ),
           skills: [
             "Kotlin",
@@ -42,14 +57,12 @@ class PrivateRepository {
         ),
         PrivateCreatedServiceEntity(
           title: "まかせてチョイス！",
-          detail: "メンバーを自動で組み分けてくれる、アプリ。\n"
-              "スポーツ、仕事、ゲームなど、様々なシーンでチーム分けというのが必要だと思います。\n"
-              "ただし、チーム分けするといっても、時間がかかってしまうのが現状です。\n"
-              "そこで、本アプリケーションを使えば全自動かつ一瞬でチーム分けが可能です。\n"
-              "メンバーを登録後、組み分け数を選択して「チョイス」するだけで簡単にチーム分けができます。",
+          detail: "チーム分けアプリです！",
           link: LinkEntity(
-            imageUrl: "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
-            url: "https://play.google.com/store/apps/details?id=io.github.emusute1212.makasetechoice",
+            imageUrl:
+                "https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png",
+            url:
+                "https://play.google.com/store/apps/details?id=io.github.emusute1212.makasetechoice",
           ),
           skills: [
             "Kotlin",
